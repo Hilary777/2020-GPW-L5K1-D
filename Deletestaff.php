a8a74b0b750ab9d350a1fafc6a9fec82497044ef
beckyzhang123 <45254158+beckyzhang123@users.noreply.github.com>
2020-05-22 11:26:55 +0800

    Add files via upload

---
+++ Deletestaff.php
@@ -0,0 +1,31 @@
+<?php
+
+require('db.php');
+
+
+// Test if connection is ok
+if (mysqli_connect_errno()) {
+    die("Database connection failed: " .
+        mysqli_connect_error() .
+        " (" . mysqli_connect_errno() . ")" 
+    );
+}
+
+$deleteid = $_GET['id'];
+
+// 2. Do a query
+$query  = "DELETE FROM Staff "; 
+$query .= "WHERE id = $deleteid";
+
+$result = mysqli_query($connection, $query);
+
+if (!$result) {
+    die("query is wrong");
+}
+
+Echo "Staff deleted";
+
+// 5. close db connection
+mysqli_close($connection);
+
+?>
